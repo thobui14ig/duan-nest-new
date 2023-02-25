de7b96706c3ba322e372251512b8bae0059c5ce6
thobui14ig <buithanhtho14ig@gmail.com>
2023-02-25 17:16:17 +0700

    252/2

--- src/gatewaies/app.gateway.ts
+++ src/gatewaies/app.gateway.ts
@@ -3,6 +3,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
@@ -15,9 +16,10 @@ import { UsersService } from './../users/users.service';
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  public listUser = [];
   constructor(
     private jwtService: JwtService,
-    private userService: UsersService,
+    private userService: UsersService, // private listUser: { id: string; token: string }[],
   ) {}
 
   @WebSocketServer() server: Server;
@@ -29,12 +31,44 @@ export class AppGateway
 
   async handleConnection(client: Socket) {
     const user = await this.getDataUserFromToken(client);
-    console.log(111, user);
-    console.log(`Client ${client.id} connected`);
+    if (user) {
+      this.addUser({ id: user._id, socketId: client.id });
+    }
+  }
+
+  @SubscribeMessage('sendMessage')
+  messages(
+    client: Socket,
+    payload: { senderId: string; text: string; receiveId: string },
+  ) {
+    const { senderId, receiveId, text } = payload;
+    const userReceive = this.getUser(receiveId);
+    const emit = this.server;
+
+    emit.emit('sendDataServer', {
+      render: true,
+    });
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client ${client.id} disconnected`);
+    this.removeUser(client.id);
+  }
+
+  addUser(user: { id: string; socketId: string }) {
+    const { id, socketId } = user;
+    !this.listUser.some((user) => user.id.toString() === id.toString()) &&
+      this.listUser.push({ id: id.toString(), socketId });
+  }
+
+  removeUser(socketId: string) {
+    this.listUser = this.listUser.filter((user) => user.socketId !== socketId);
+  }
+
+  getUser(userId: string) {
+    const data = this.listUser.find((user) => {
+      user.id.toString() === userId;
+    });
+    return data;
   }
 
   async getDataUserFromToken(client: Socket): Promise<any> {
@@ -44,7 +78,6 @@ export class AppGateway
 
       return await this.userService.findByName(decoded.name); // response to function
     } catch (ex) {
-      console.log(222);
       // throw new HttpException('Not found', HttpStatus.NOT_FOUND);
     }
   }
